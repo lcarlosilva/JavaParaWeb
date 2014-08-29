54604211118786464afc807bb76b3ab4dea34d1e
lcarlosilva <luizcarlos.arrudasilva@gmail.com>
2014-08-29 14:55:32 -0300

    Criação e mapeamento da classe Usuario, e alteração na classe de exceção UtilExecption.

--- FinanceiroWeb/src/financeiro/util/UtilExecption.java
+++ FinanceiroWeb/src/financeiro/util/UtilExecption.java
@@ -10,23 +10,19 @@ public class UtilExecption extends Exception {
 
 	public UtilExecption(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public UtilExecption(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public UtilExecption(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public UtilExecption(String message, Throwable cause,
 			boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
 	}
 
 }
