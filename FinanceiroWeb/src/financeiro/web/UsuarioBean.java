a30ebcb36bc9e4b39e25d419c302a2687a67cab6
lcarlosilva <luizcarlos.arrudasilva@gmail.com>
2014-09-03 15:02:48 -0300

    atualização do usuario.xhtml e usuarioBean

--- FinanceiroWeb/src/financeiro/web/UsuarioBean.java
+++ FinanceiroWeb/src/financeiro/web/UsuarioBean.java
@@ -1,9 +1,12 @@
 package financeiro.web;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 
 import financeiro.usuario.Usuario;
+import financeiro.usuario.UsuarioRN;
 
 @ManagedBean(name="usuarioBean")
 @RequestScoped
@@ -11,6 +14,28 @@ public class UsuarioBean {
 
 	private Usuario usuario = new Usuario();
 	private String confirmarSenha;
+	
+	public String novo(){
+		this.usuario = new Usuario();
+		this.usuario.setAtivo(true);
+		return "usuario";
+	}
+	
+	public String salvar() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		
+		String senha = this.usuario.getSenha();
+		if (!senha.equals(this.confirmarSenha)) {
+			FacesMessage facesMessage = new FacesMessage("A senha não foi confirmada corretamente!");
+			context.addMessage(null, facesMessage);
+			return null;
+		}
+		
+		UsuarioRN usuarioRN = new UsuarioRN();
+		usuarioRN.salvar(this.usuario);
+		
+		return "usuarioSucesso";
+	}
 
 	public Usuario getUsuario() {
 		return usuario;
